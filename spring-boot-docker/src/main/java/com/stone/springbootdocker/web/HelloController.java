f06624327c6a66d7e2527c9e0ea19f349a146717
honor_stone <honor_stone@163.com>
2019-07-31 14:36:24 +0800

    更新Api接口功能

--- spring-boot-docker/src/main/java/com/stone/springbootdocker/web/HelloController.java
+++ spring-boot-docker/src/main/java/com/stone/springbootdocker/web/HelloController.java
@@ -3,6 +3,9 @@ package com.stone.springbootdocker.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * @author honorstone
  * @date 2019/3/11 22:12
@@ -13,5 +16,12 @@ public class HelloController {
     @GetMapping(value = {"/", "/index"})
     public String index() {
         return "Hello Docker!";
+    }
+
+
+    @GetMapping("/now")
+    public String now(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
     }
 }
